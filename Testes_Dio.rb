95995c72757ca759543d14f76ee220ebcffc7767
Edmilson-Lopes <milsonlopes@hotmail.com>
2023-03-10 20:54:11 -0300

    Testes Private e Protected

--- Testes_Dio.rb
+++ Testes_Dio.rb
@@ -1,12 +1,42 @@
 
-class Pessoa
+class MinhaClasse
+    def m1
+        puts "Método 1"
+        
+    end
 
-    def self.gerar
-        p "vou fazer algo antes"
-        Pessoa.new
+    private
+    
+    def m2
+        puts "Método 2"
+    end
+
+    def m3
+        puts "Método 3 Privado"
+    end
+
+protected
+
+    def m5
+        puts "Método 5"
+    end
+
+end
+
+
+
+
+class MinhaSubClasse < MinhaClasse
+    def m4
+        m3
+        outro_obj = MinhaClasse.new
+        puts "Método 4"
+        outro_obj.m5
     end
 end
 
-pessoa = Pessoa.gerar
 
-p pessoa
+###
+
+objeto = MinhaSubClasse.new
+objeto.m4
