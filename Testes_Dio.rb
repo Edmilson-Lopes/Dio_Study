74b3de86eac3a20bc8d7b11779e2e187fc49ce74
Edmilson-Lopes <milsonlopes@hotmail.com>
2023-03-29 20:59:21 -0300

    Testes Dio

--- Testes_Dio.rb
+++ Testes_Dio.rb
@@ -1,41 +1,27 @@
-# Algoritimo de procura binária
 
-array = [5, 10, 12, 15, 30, 45, 60, 80, 100]
-
-numero_alvo = 12
-
-
-def binary_search(array, numero_alvo)
-
-    left = 0
-
-    right = array.length-1
-
-    while left <= right
-        mid = (left + right) / 2
-
-        if array[mid] == numero_alvo 
-            return mid
-
-        elsif array[mid] < numero_alvo
-            left = mid +1
-
-        else 
-            right = mid -1
-        end
-
+class ContaBancaria
+    def initialize(proprietario, valor_inicial)
+        @proprietario = proprietario
+        @valor        = valor_inicial
     end
 
-    return -1
+    def transferir (outra_conta, valor)
+        if saldo >= valor 
+        debitar(valor)
+        else
+            p "Não consigo transferir, saldo insufiviente"
+    end
+
+    def saldo
+        @valor
+    end
 
 end
 
-resultado = binary_search(array, numero_alvo)
+private 
 
-if resultado == -1
-    puts "O número não está na lista"
-
-else
-    puts "O número está no índice #{resultado} da lista"
-
+def debitar(valor)
+    @valor -= valor 
 end
+
+
